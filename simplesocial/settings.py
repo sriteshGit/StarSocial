addcb5b79d678c90de9794cd41f45454706786c6
Ritesh Srivastava <riteshsrivastava123456@gmail.com>
2021-11-10 01:05:58 +0530

    heroku

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
